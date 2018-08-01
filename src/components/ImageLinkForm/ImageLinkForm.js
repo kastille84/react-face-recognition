ec97034465558a58d63f9f640725e2e7628b3917
Edwin Martinez <kastille84@gmail.com>
2018-08-01 13:17:18 -0400

    Added Major Components

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+    return (
+        <div>
+            <p className="f3">
+                {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+            </p>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
